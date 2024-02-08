2af459fdfcd14c0677bbaf4091023d89b0e6438e
Ashish <ashishbhopte50@gmail.com>
2024-02-08 15:20:42 +0530

    .080224

--- static/BKB.js
+++ static/BKB.js
@@ -1,13 +1,26 @@
 
-
-let btnClear = document.getElementById('button-ref')
-let inputs = document.querySelectorAll('input')
-
-btnClear.addEventListenerbtnClear('click', ()=>
-{
-
-var word = "Hello, World!";
-console.log(word);
-/*above 2 line added for tesing purpose*/
-inputs.forEach(input=> input.value = '');
-})
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
