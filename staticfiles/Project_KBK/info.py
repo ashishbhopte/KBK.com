383fc21fe501a81c8d38cc7d9810e88cdb568a8f
Ashish <ashishbhopte50@gmail.com>
2024-03-05 17:49:14 +0530

    050324 Workin on static files

--- staticfiles/Project_KBK/info.py
+++
@@ -1,5 +0,0 @@
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT= 587
-EMAIL_HOST_USER='bkbseoandweb@gmail.com'
-EMAIL_HOST_PASSWORD='mrssnnwruwbjugah'
-EMAIL_USE_TLS=True
--- staticfiles/staticfiles/Project_KBK/views.py
+++
@@ -1,2 +0,0 @@
-# from django.shortcuts import render
-# from BKB_Home.models import KBKform
